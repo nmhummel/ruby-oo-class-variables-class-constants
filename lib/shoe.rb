90cc6168e46ff63af53705abcfd570f567af9411
nmhummel <natalie.hummel@gmail.com>
2020-11-02 17:28:40 -0600

    commit tree 5598847d8064b7bc512b8aead6285a1217aa8283

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,8 +2,18 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
+  BRANDS = [] # new
+
   def initialize(brand)
     @brand = brand
+    if BRANDS.include?(brand) # new
+    else BRANDS << brand  # new
+    end # new
+  end
+
+  # new
+  def brand=(brand)
+    BRANDS
   end
 
   def cobble
@@ -12,3 +22,4 @@ class Shoe
   end
 
 end
+
